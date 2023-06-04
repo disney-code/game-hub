92e73685a7507ce7d0f063d95da97b9e74dff2cd
Charlene <charlenetansijie@gmail.com>
2023-06-05 00:29:25 +0800

    Display Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import {List,ListItem,HStack,Image,Text} from '@chakra-ui/react'
 import useGenres from '../hooks/useGenre'
+import getCroppedImageUrl from '../services/image-url';
 
 
 const GenreList = () => {
 	const {data}=useGenres();
   return (
-   <ul>
-	{data.map(d=><li key={d.id}>{d.name}</li>)}
-   </ul>
+   <List>
+	{data.map(d=><ListItem key={d.id} paddingY='5px' >
+		<HStack>
+		<Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(d.image_background)}/>
+		<Text fontSize='lg'>{d.name}</Text>
+		
+		</HStack></ListItem>)}
+   </List>
   )
 }
 
